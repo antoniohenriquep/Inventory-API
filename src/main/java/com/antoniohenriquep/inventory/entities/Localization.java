0f40f3a96137c3a55324a3cb56972ecd7bb91b25
antoniohenriquep <antoniohenriquep@outlook.com>
2025-01-17 14:33:34 -0300

    feat: Defining Localization/Department relationship

--- src/main/java/com/antoniohenriquep/inventory/entities/Localization.java
+++ src/main/java/com/antoniohenriquep/inventory/entities/Localization.java
@@ -1,4 +1,17 @@
 package com.antoniohenriquep.inventory.entities;
 
+import com.antoniohenriquep.inventory.Department;
+import jakarta.persistence.*;
+
+@Entity
 public class Localization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
 }
