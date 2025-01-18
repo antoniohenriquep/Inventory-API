ca86b1ae6e8102e908e4fd4ddf9c795fd3b370c9
antoniohenriquep <antoniohenriquep@outlook.com>
2025-01-18 18:21:35 -0300

    feat: Adding some entities constructors, getters, setters

--- src/main/java/com/antoniohenriquep/inventory/Department.java
+++ src/main/java/com/antoniohenriquep/inventory/Department.java
@@ -4,6 +4,7 @@ import com.antoniohenriquep.inventory.entities.Localization;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 public class Department {
@@ -16,4 +17,42 @@ public class Department {
     @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Localization> localizations;
 
+    public Department() {}
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Localization> getLocalizations() {
+        return localizations;
+    }
+
+    public void addLocalizations(Localization localization) {
+        localizations.add(localization);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/antoniohenriquep/inventory/entities/Grouping.java
+++ src/main/java/com/antoniohenriquep/inventory/entities/Grouping.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Grouping {
 
@@ -14,5 +16,34 @@ public class Grouping {
 
     private String name;
 
+    public Grouping() {}
 
+    public Grouping(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Grouping grouping = (Grouping) o;
+        return Objects.equals(id, grouping.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
